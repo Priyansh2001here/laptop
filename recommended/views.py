3cea9ed25cce67358ac1ca725b93fa33907e6da8
Lenovo <singhpriyansh51001@gmail.com>
2020-04-24 13:15:57 +0530

    New Files

--- recommended/views.py
+++ recommended/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Category
 from django.http import HttpResponse
+from best_laptops.models import Laptop
 
 
 # Create your views here.
@@ -12,9 +13,17 @@ def categories(request):
 def show_products(request, laptop_category):
     category = Category.objects.filter(name=laptop_category)  # Query Set
     category = category[0]  # Category
+    print(type(category))
     return render(request, 'products.html', {'category': category})
 
 
 def show_laptop(request, req_laptop, laptop_category):
-    x = 8
-    return HttpResponse("Showing laptop" + req_laptop)
+    laptop = Laptop.objects.filter(laptop_name=req_laptop)
+    laptop = laptop[0]
+    # req_laptop_name = laptop.laptop_name
+    # req_laptop_category = laptop.category
+    # req_laptop_cost = laptop.cost
+    # req_laptop_serial = laptop.laptop_serial
+    # req_laptop_image = laptop.laptop_image
+    # req_laptop_n = laptop.company
+    return render(request, 'product_details.html', {"laptop_details": laptop})
