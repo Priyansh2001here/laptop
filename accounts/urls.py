253a535285a37dbd63324eb866148e13a4b03dff
Lenovo <singhpriyansh51001@gmail.com>
2020-04-25 18:27:17 +0530

    New Files

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
 
-
+app_name='accounts'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout', views.logout, name='logout'),
+    path('friend/', views.friend_opt, name='frient_opt'),
+    path('friend/<str:operation>', views.show_friend, name='friend'),
+    path('add', views.add_page, name='add_page'),
+    path('add/<str:to_user>', views.add, name='add_friend'),
+    path('<str:req_from>', views.accept, name='accept'),
+    path('friend/remove/<str:which_user>', views.remove, name='remove')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 from django.contrib.auth import authenticate
+from friendship.models import Friend, Follow, Block
+from django.http import HttpResponse
+from friendship.models import FriendshipRequest
 
 
 # Create your views here.
@@ -22,7 +25,8 @@ def register(request):
                 messages.info(request, "An User With This email is already registered!!!")
                 return redirect('register')
             else:
-                user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, password=password1, username=username)
+                user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email,
+                                                password=password1, username=username)
                 user.save()
                 print("\n--- User Created ---\n")
         else:
@@ -37,7 +41,7 @@ def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        print("\n\n"+username + "," + password+"\n\n")
+        print("\n\n" + username + "," + password + "\n\n")
         user = authenticate(request, username=username, password=password)
         if user is None:
             messages.info(request, "Invalid Credentials !!")
@@ -52,3 +56,45 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('/')
+
+
+def show_friend(request, operation):
+    if operation == 'show':
+        friends = Friend.objects.friends(request.user)
+        unread_friend_requests = Friend.objects.unread_requests(user=request.user)
+        return render(request, 'show_friends.html',
+                      {"friends": friends, "unread_friend_requests": unread_friend_requests})
+    if operation == 'requests':
+        requests = FriendshipRequest.objects.filter(to_user=request.user)
+        requests = [i.from_user for i in requests]
+        return render(request, "requests.html", {"requests": requests})
+
+
+def friend_opt(request):
+    return render(request, "friend_opt.html")
+
+
+def add_page(request):
+    users = User.objects.all()
+    return render(request, "add_friend.html", {"users": users})
+
+
+def add(request, to_user):
+    other_user = User.objects.get(username=to_user)
+    Friend.objects.add_friend(from_user=request.user, to_user=other_user, message="I want to be friend with you")
+    return HttpResponse("Friend Request has been sent")
+
+
+def accept(request, req_from):
+    user = User.objects.get(username=req_from)
+    print(user)
+    friend_requests = FriendshipRequest.objects.filter(from_user=user)
+    print(friend_requests)
+    friend_requests[0].accept()
+    return show_friend(request, operation="requests")
+
+
+def remove(request, which_user):
+    user = User.objects.get(username=which_user)
+    Friend.objects.remove_friend(request.user, user)
+    return show_friend(request, operation="show")
