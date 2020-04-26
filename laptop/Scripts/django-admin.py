a4b79f2eac3d7197607d579b1e3ed2bf02fd2b20
Lenovo <singhpriyansh51001@gmail.com>
2020-04-26 18:13:27 +0530

    Inbox

---
+++ laptop/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Lenovo\Desktop\New folder (6)\laptop\laptop\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
